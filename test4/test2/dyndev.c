c5ab131ef79ac42818aeb0759e654445ade20546
ShajTester <kolobrod2002@yandex.ru>
2024-01-01 18:46:30 +0300

    test 4 ~ test 2

---
+++ test4/test2/dyndev.c
@@ -0,0 +1,105 @@
+
+#include "dev.h"
+#include <complex.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/version.h>
+#include <linux/init.h>
+
+static int major = 0;
+module_param(major, int, S_IRUGO);
+
+#define EOK 0
+static int device_open = 0;
+
+static int dev_open(struct inode *n, struct file *f)
+{
+    if(device_open) return -EBUSY;
+    device_open++;
+    return EOK;
+}
+
+static int dev_release(struct inode *n, struct file *f)
+{
+    device_open--;
+    return EOK;
+}
+
+static const struct file_operations dev_fops = {
+    .owner = THIS_MODULE,
+    .open = dev_open,
+    .release = dev_release,
+    .read = dev_read,
+};
+
+#define DEVICE_FIRST 0
+#define DEVICE_COUNT 3
+#define MODNAME "my_dyndev_dev"
+
+static struct cdev hcdev;
+static struct class *devclass;
+
+static int __init dev_init(void)
+{
+    int ret;
+    int i;
+    dev_t dev;
+
+    if(major != 0)
+    {
+        dev = MKDEV(major, DEVICE_FIRST);
+        ret = register_chrdev_region(dev, DEVICE_COUNT, MODNAME);
+    }
+    else
+    {
+        ret = alloc_chrdev_region(&dev, DEVICE_FIRST, DEVICE_COUNT, MODNAME);
+        major = MAJOR(dev);
+    }
+    if(ret < 0)
+    {
+        printk(KERN_INFO "=== Can not register char device region\n");
+        goto err;
+    }
+    cdev_init(&hcdev, &dev_fops);
+    hcdev.owner = THIS_MODULE;
+    ret = cdev_add(&hcdev, dev, DEVICE_COUNT);
+    if(ret < 0)
+    {
+        unregister_chrdev_region(MKDEV(major, DEVICE_FIRST), DEVICE_COUNT);
+        printk(KERN_ERR "=== Can not add char device\n");
+        goto err;
+    }
+
+    devclass = class_create(THIS_MODULE, "dyn_class");
+    for(i = 0; i < DEVICE_COUNT; i++)
+    {
+#define DEVNAME "dyn"
+        dev = MKDEV(major, DEVICE_FIRST + i);
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,26)
+        device_create(devclass, NULL, dev, "%s_%d", DEVNAME, i);
+#else
+        device_create(devclass, NULL, dev, NULL, "%s_%d", DEVNAME, i);
+#endif
+    }
+
+    printk(KERN_INFO "=== module installed %d:%d === \n", MAJOR(dev), MINOR(dev));
+
+err:
+    return ret;
+}
+
+static void __exit dev_exit(void)
+{
+    dev_t dev;
+    int i;
+    for(i=0; i < DEVICE_COUNT; i++)
+    {
+        dev = MKDEV(major, DEVICE_FIRST + i);
+        device_destroy(devclass, dev);
+    }
+    class_destroy(devclass);
+    cdev_del(&hcdev);
+    unregister_chrdev_region(MKDEV(major, DEVICE_FIRST), DEVICE_COUNT);
+    printk(KERN_INFO "=== module removed ===\n");
+}
+
